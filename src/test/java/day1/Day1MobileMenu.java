97fc2e6a4df0135e8cf8ba38e512177664950f12
Atish <ab245324@gmail.com>
2019-08-08 20:28:19 +0530

    Complete Day1 Done

--- src/test/java/day1/Day1MobileMenu.java
+++ src/test/java/day1/Day1MobileMenu.java
@@ -1,10 +1,14 @@
 package day1;
 
+import static org.testng.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
-
+import pageObjects.HomePage;
 import resources.Base;
 
 public class Day1MobileMenu extends Base {
@@ -15,50 +19,29 @@ public class Day1MobileMenu extends Base {
 		
 		driver = initializeDriver();
 		driver.get(prop.getProperty("URL"));
-		driver.get("http://live.guru99.com/index.php/");
-		//get Url
+		//1. get Url
+		driver.get("http://live.guru99.com");
 		
-		//verify title
-		
-		//WebElement title_Home=driver.findElement(By.xpath("//*[@id=\"top\"]/body/div/div/div[2]/div/div[1]/div/div/h2/text()"));
-		//title_Home.getText().contains("THIS IS DEMO SITE");
-		String actual_title= driver.getTitle();
-		String expected_title="THIS IS DEMO  SITE";
-		
-		if(actual_title.equalsIgnoreCase(expected_title))
-		{
-			System.out.println("title matched");
-		}
-		else
-		{
-			System.out.println("title not matched");
-		}
-		
-		//click on MOBILE
-		
-		driver.findElement(By.xpath("//*[@id=\"nav\"]/ol/li[1]/a")).click();
-		//verify title
-		
-		String act_title=driver.getTitle();
-		String exp_title="MOBILE";
-		if(act_title.equalsIgnoreCase(exp_title))
-		{
-			System.out.println("title matched");
-		}
-		else
-		{
-			System.out.println("title not matched");
-		}
-		//Sort by name
-		
-		Select s= new Select(driver.findElement(By.xpath("//*[@id=\"top\"]/body/div/div/div[2]/div/div[2]/div[1]/div[3]/div[1]/div[1]/div/select")));
-		s.selectByVisibleText("Name");
-	
-		
-	}
-	
-	
-	
-	
+		//2. verify Home page title
+		String homePage= driver.getTitle();
+		assertEquals(homePage, "Home page");
 
+		//3. Check Text on Home page
+		String text = driver.findElement(By.xpath("//*[@id='top']/body/div/div/div[2]/div/div[1]/div/div/h2")).getText();
+		assertEquals(text.trim(), "THIS IS DEMO SITE FOR");
+		
+		
+		//4. verify Mobile page title
+		HomePage l = new HomePage(driver);
+		l.mobile().click();
+		String mobileTitle= driver.getTitle();
+		assertEquals(mobileTitle, "Mobile");
+		
+		
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		//5. Sort by Name 
+		WebElement sortBy = driver.findElement(By.xpath("//select[@title='Sort By']"));
+		Select s= new Select(sortBy);
+		s.selectByVisibleText("Name");
+	}
 }
